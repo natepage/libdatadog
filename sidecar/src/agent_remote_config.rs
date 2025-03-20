3fb9aee46515f2e1620fe76657d2940786503293
nathanpage <nathan.page@eonx.com>
2025-03-20 12:16:54 +1100

    Prefix sidecar path with /tmp to see if it helps in AWS Lambda

--- sidecar/src/agent_remote_config.rs
+++ sidecar/src/agent_remote_config.rs
@@ -30,7 +30,7 @@ fn path_for_endpoint(endpoint: &Endpoint) -> CString {
 
     #[allow(clippy::unwrap_used)]
     CString::new(format!(
-        "/ddcfg-{}-{}", // short enough because 31 character macos limitation
+        "/tmp/ddcfg-{}-{}", // short enough because 31 character macos limitation
         primary_sidecar_identifier(),
         hasher.finish()
     ))
